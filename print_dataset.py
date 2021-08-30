ab4c3a717f4c4dbb31be0117f3aeb52cbd937b73
yashchks87 <ychoksi@calstatela.edu>
2021-08-29 18:35:43 -0700

    Files added.

---
+++ print_dataset.py
@@ -0,0 +1,23 @@
+import PIL
+def show_dataset(thumb_size, cols, rows, ds):
+    mosaic = PIL.Image.new(mode='RGB', size=(thumb_size*cols + (cols-1), 
+                                             thumb_size*rows + (rows-1)))
+   
+    for idx, data in enumerate(iter(ds)):
+        try:
+          img, target_or_imgid = data
+        except:
+          img = data
+        ix  = idx % cols
+        iy  = idx // cols
+        img = np.clip(img.numpy() * 255, 0, 255).astype(np.uint8)
+        img = PIL.Image.fromarray(img)
+        img = img.resize((thumb_size, thumb_size), resample=PIL.Image.BILINEAR)
+        mosaic.paste(img, (ix*thumb_size + ix, 
+                           iy*thumb_size + iy))
+
+
+    display(mosaic)
+
+if '__name__' == '__main__':
+    show_dataset()
---
+++ tpu_starter.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+def tpu_start(device_name):
+    device = device_name
+    # TPU and GPU mechanism
+    if device == 'TPU':
+        print('Connecting to TPU')
+        try:
+            # Detects weather system has TPU.
+            tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+            print('Running on TPU!', tpu.master())
+        except ValueError:
+            print('Could not connect to TPU.')
+            tpu = None
+        if tpu:
+            try:
+                print('Starting TPU')
+                # Connects to the TPU cluster
+                tf.config.experimental_connect_to_cluster(tpu)
+                # Starts TPU cluster
+                tf.tpu.experimental.initialize_tpu_system(tpu)
+                # Object gives TPU cluster with 7 replicas
+                strategy = tf.distribute.experimental.TPUStrategy(tpu)
+                print('TPU started.')
+            except:
+                print('Failed to start TPU.')
+        else:
+            device = 'GPU'
+    # If no TPU then...
+    if device != 'TPU':
+        print('Using single GPU and CPU.')
+        strategy = tf.distribute.get_strategy()
+    
+    if device == 'GPU':
+        print(f'Number of GPU available: {len(tf.config.experimental.list_physical_devices("GPU"))}')
+
+    AUTO = tf.data.experimental.AUTOTUNE
+    REPLICAS = strategy.num_replicas_in_sync
+    print(f'Replicas: {REPLICAS}')
+    print('Return auto, replica and strategy object in the same order.')
+    return AUTO, REPLICAS, strategy
+
+if '__name__' == '__main__':
+    tpu_start()
+    
