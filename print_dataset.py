48b9a80d17c7891cdad77fcbdcfa0222ab51f2ce
Yash Choksi <ychoksi@calstatela.edu>
2022-04-27 19:07:08 -0700

    Print dataset file fixed with indentation issue as well.

--- print_dataset.py
+++ print_dataset.py
@@ -12,27 +12,27 @@ def show_dataset(thumb_size, cols, rows, ds):
                                          thumb_size*rows + (rows-1)))
    
     # For loop to iterate over images
-    for idx, data in enumerate(iter(ds)):
-        # Try block which will look at images and de couple if they have image and id both
-        try:
-          img, target_or_imgid = data
-        # If only image is present it will go to except block
-        except:
-          img = data
-        # to get co-ordinate position ix will look at x axis and iy will look at y axis
-        ix  = idx % cols
-        iy  = idx // cols
-        # Get the image and change data type to uint8
-        img = np.clip(img.numpy() * 255, 0, 255).astype(np.uint8)
-        img = PIL.Image.fromarray(img)
-        # image resizing upto the given fixed size
-        img = img.resize((thumb_size, thumb_size), resample=PIL.Image.BILINEAR)
-        # paste created image to the given x-y place with given size.
-        mosaic.paste(img, (ix*thumb_size + ix, 
-                           iy*thumb_size + iy))
+  for idx, data in enumerate(iter(ds)):
+    # Try block which will look at images and de couple if they have image and id both
+    try:
+      img, target_or_imgid = data
+      # If only image is present it will go to except block
+    except:
+      img = data
+      # to get co-ordinate position ix will look at x axis and iy will look at y axis
+      ix  = idx % cols
+      iy  = idx // cols
+      # Get the image and change data type to uint8
+      img = np.clip(img.numpy() * 255, 0, 255).astype(np.uint8)
+      img = PIL.Image.fromarray(img)
+      # image resizing upto the given fixed size
+      img = img.resize((thumb_size, thumb_size), resample=PIL.Image.BILINEAR)
+      # paste created image to the given x-y place with given size.
+      mosaic.paste(img, (ix*thumb_size + ix, 
+                         iy*thumb_size + iy))
 
 
-    display(mosaic)
+  display(mosaic)
 
 if '__name__' == '__main__':
     show_dataset()
