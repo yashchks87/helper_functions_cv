35bbbd3379c101f58bb7d714b39c9dcf26f40106
Yash Choksi <ychoksi@calstatela.edu>
2022-06-10 13:13:55 -0700

    Resnet file is fixed.

--- pytorch_version/resnet_blocks.py
+++ pytorch_version/resnet_blocks.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 from functools import partial
 from dataclasses import dataclass
 from collections import OrderedDict
-from collections import OrderedDict
 
 
 class Conv2dAuto(nn.Conv2d):
@@ -31,6 +30,27 @@ class ResidualBlock(nn.Module):
     @property
     def should_apply_shortcut(self):
         return self.in_channels != self.out_channels
+
+class ResNetResidualBlock(ResidualBlock):
+    def __init__(self, in_channels, out_channels, expansion=1, downsampling=1, conv=conv3x3, *args, **kwargs):
+        super().__init__(in_channels, out_channels)
+        self.expansion, self.downsampling, self.conv = expansion, downsampling, conv
+        self.shortcut = nn.Sequential(OrderedDict(
+        {
+            'conv' : nn.Conv2d(self.in_channels, self.expanded_channels, kernel_size=1,
+                      stride=self.downsampling, bias=False),
+            'bn' : nn.BatchNorm2d(self.expanded_channels)
+            
+        })) if self.should_apply_shortcut else None
+        
+        
+    @property
+    def expanded_channels(self):
+        return self.out_channels * self.expansion
+    
+    @property
+    def should_apply_shortcut(self):
+        return self.in_channels != self.expanded_channels
 
 def conv_bn(in_channels, out_channels, conv, *args, **kwargs):
     return nn.Sequential(OrderedDict({'conv': conv(in_channels, out_channels, *args, **kwargs), 
