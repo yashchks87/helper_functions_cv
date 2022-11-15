4224fba04280425784dccd5d526aaf41934714e2
yashchks87 <yashchks87@gmail.com>
2022-11-15 21:04:27 +0000

    Learning rate schuduler fixed.

--- tensorflow_helpers/change_learning_rate_epoch.py
+++ tensorflow_helpers/change_learning_rate_epoch.py
@@ -1,21 +1,21 @@
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow.keras.backend as K
+
 # This class generates callback class which is called during model.fit method
-class CallbackForSavingModelWeights(keras.callbacks.Callback):
-    # Constructor takes arguement as path where weights need to be stored.
-    def __init__(self, path, change_rate):
-        # Boolean variable which will store value weather we like to change 
-        # learning rate or not
-        self.change_rate = change_rate
+class ChangeLR(keras.callbacks.Callback):
+    def __init__(self, dict_):
+        epochs, lrs = list(dict_.keys()), list(dict_.values())
+        self.new_dict = {}
+        for x in range(len(epochs)):
+            if '_' in epochs[x]:
+                min_, max_ = epochs[x].split('_')
+                min_, max_ = int(min_), int(max_)
+                for y in range(min_, max_+1):
+                    self.new_dict[y] = lrs[x]
     def on_epoch_begin(self, epoch, logs):
-        if self.change_rate == True:
-            # For first 3 epochs 
-            if 0 <= epoch <= 3:
-                K.set_value(self.model.optimizer.learning_rate, 0.1)
-            # For 4 to 20 epochs
-            elif 4 <= epoch <= 20:
-                K.set_value(self.model.optimizer.learning_rate, 0.01)
-            # For 20 to 50 epochs
-            elif 20 <= epoch <= 50:
-                K.set_value(self.model.optimizer.learning_rate, 0.001)
-            # For 50 to 100 epochs
-            elif 50 <= epoch <= 100:
-                K.set_value(self.model.optimizer.learning_rate, 0.0001)
+        K.set_value(self.model.optimizer.learning_rate, self.new_dict[epoch])
+        print(self.model.optimizer.learning_rate)
+
+if __name__ == '__main__':
+    ChangeLR()
