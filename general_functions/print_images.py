d1c6863f8e61fa34da3042596fbc9e830af94301
yashchks87 <yashchks87@gmail.com>
2023-09-04 18:23:06 +0000

    General photo priting is added.

---
+++ general_functions/print_images.py
@@ -0,0 +1,22 @@
+import numpy as np
+from PIL import Image
+import PIL
+
+def print_images(files, thumb_size, rows, cols):
+    mosaic = Image.new(
+        mode="RGB",
+        size=(thumb_size * cols + (cols - 1), thumb_size * rows + (rows - 1)),
+    )
+
+    for idx, path in enumerate(files):
+        img = Image.open(path)
+        img = np.array(img)
+        ix = idx % cols
+        iy = idx // cols
+        img = np.clip(img, 0, 255).astype(np.uint8)
+        img = Image.fromarray(img)
+        img = img.resize((thumb_size, thumb_size), resample=PIL.Image.BILINEAR)
+        mosaic.paste(img, (ix * thumb_size + ix, iy * thumb_size + iy))
+    
+    display(mosaic)
+
