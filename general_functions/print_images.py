8ff3e12892606e8ccf5b64574182975a77590900
yashchks87 <yashchks87@gmail.com>
2023-09-04 18:39:45 +0000

    Save file utility is working.

--- general_functions/print_images.py
+++ general_functions/print_images.py
@@ -2,7 +2,7 @@ import numpy as np
 from PIL import Image
 import PIL
 
-def print_images(files, thumb_size, rows, cols):
+def print_images(files, thumb_size, rows, cols, save = False, save_path = None):
     mosaic = Image.new(
         mode="RGB",
         size=(thumb_size * cols + (cols - 1), thumb_size * rows + (rows - 1)),
@@ -19,4 +19,8 @@ def print_images(files, thumb_size, rows, cols):
         mosaic.paste(img, (ix * thumb_size + ix, iy * thumb_size + iy))
     
     display(mosaic)
+
+    if save:
+        if save_path is not None:
+            mosaic.save(save_path)
 
