1dd004de293077939ef3297d4c15abcc24faa77c
yashchks87 <yashchks87@gmail.com>
2023-09-22 03:02:13 +0000

    New data and clean up is made.

--- general_functions/plot_detection.py
+++ general_functions/plot_detection.py
@@ -3,6 +3,7 @@ import cv2, PIL
 import matplotlib.pyplot as plt
 
 def plot_images(img_list, box_list, fig_size, box_color, thickness, rows, cols):
+    # This function takes in img list with box list and print image dataset.
     plt.figure(figsize=fig_size)
     for x in range(len(img_list)):
         img = img_list[x]
@@ -10,23 +11,7 @@ def plot_images(img_list, box_list, fig_size, box_color, thickness, rows, cols):
         box = [float(x.strip()) for x in box.split(',')]
         img = cv2.imread(img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        # if np.sum(img / 255.0) != 3.0:
         img = cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 0, 255), thickness=thickness)
         ax = plt.subplot(4, 10, x+1)
         plt.imshow(img)
         plt.axis("off")
-        # else:
-        #     ax = plt.subplot(4, 10, x+1)
-        # ax = plt.subplot(4, )
-
-
-
-
-
-
-
-
-#  ax = plt.subplot(4, BATCH_SIZE//4, i + 1)
-#         label = labels[0][i]
-#         box = (labels[1][i] * input_size)
-#         box = tf.cast(box, tf.int32)
